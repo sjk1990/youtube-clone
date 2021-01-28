db584fe7626a0f274bae3c5f558970b9f3f58d48
SJKim <worldsjk1990@gmail.com>
2021-01-28 21:43:04 +0900

    apply .env for api key

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./app.css";
 import VideoList from "../src/video_list/vidoe_list";
+import dotenv from "dotenv";
+dotenv.config();
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -11,9 +13,10 @@ function App() {
       method: "GET",
       redirect: "follow",
     };
+    console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
 
     fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key={Yourkey}",
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
       requestOptions
     )
       .then((response) => response.json())
