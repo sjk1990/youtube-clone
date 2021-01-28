9edb99f17c5aff913ade82381d8ab72e1a9c0876
SJKim <worldsjk1990@gmail.com>
2021-01-28 21:10:31 +0900

    show video list using by youtube api

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "../src/video_list/vidoe_list";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [videos, setVideos] = useState([]);
+
+  //componentDidMount + componentDidUpdate , option [] : only react when Mount
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key={Yourkey}",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
