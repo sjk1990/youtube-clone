3af0cbd90f499b9299cfae7d41785e4ed910093d
SJKim <worldsjk1990@gmail.com>
2021-01-28 22:52:46 +0900

    list up thumbnail & title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import "./app.css";
 import VideoList from "../src/video_list/vidoe_list";
-import dotenv from "dotenv";
-dotenv.config();
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -13,7 +11,6 @@ function App() {
       method: "GET",
       redirect: "follow",
     };
-    console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
 
     fetch(
       `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
