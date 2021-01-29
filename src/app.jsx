6e8e6c4d5c34a3cea6df7b1b6fa54e1c621c1e78
SJKim <worldsjk1990@gmail.com>
2021-01-29 11:17:51 +0900

    refactor youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,39 +3,20 @@ import styles from "./app.module.css";
 import VideoList from "../src/component/video_list/vidoe_list";
 import SearchHeader from "./component/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
+    youtube
+      .search(query)
       .then((items) => setVideos(items))
       .catch((error) => console.log("error", error));
   };
 
   //componentDidMount + componentDidUpdate , option [] : only react when Mount
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
+    youtube
+      .mostPopular()
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
   }, []);
