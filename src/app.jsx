f2ce53ffa7decc24468f7356ffdd53abc54da0e7
SJKim <worldsjk1990@gmail.com>
2021-01-29 10:49:20 +0900

    make search header

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from "react";
-import "./app.css";
-import VideoList from "../src/video_list/vidoe_list";
+import styles from "./app.module.css";
+import VideoList from "../src/component/video_list/vidoe_list";
+import SearchHeader from "./component/search_header/search_header";
 
 function App() {
   const [videos, setVideos] = useState([]);
+
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   //componentDidMount + componentDidUpdate , option [] : only react when Mount
   useEffect(() => {
@@ -21,7 +40,12 @@ function App() {
       .catch((error) => console.log("error", error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
